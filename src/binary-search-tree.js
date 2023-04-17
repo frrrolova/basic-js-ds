18d6b6c80904d697d49a33b6138d98778eb97eb5
t_frrolova <tatianaaleks.frolova@gmail.com>
2023-04-17 13:29:26 +0200

    tree root, add, has

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,26 +1,58 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  treeRoot = null;
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let node = new Node(data);
+    if (this.treeRoot === null) {
+      this.treeRoot = node;
+      return;
+    }
+
+    let pointer = this.treeRoot;
+    while (pointer !== null) {
+      if (data > pointer.data) {
+        if (pointer.right === null) {
+          pointer.right = node;
+          pointer = null;
+        } else {
+          pointer = pointer.right;
+        }
+      } else {
+        if (pointer.left === null) {
+          pointer.left = node;
+          pointer = null;
+        } else {
+          pointer = pointer.left;
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let pointer = this.treeRoot;
+    while (pointer !== null) {
+      if (data === pointer.data) {
+        return true;
+      }
+      if (data > pointer.data) {
+        pointer = pointer.right;
+      } else {
+        pointer = pointer.left;
+      }
+    }
+    return false;
   }
 
   find(/* data */) {
